4656f8ed3738930909dee6b42831a5157125222f
Nicolas Pedicino <nicolaspablopedicino@gmail.com>
2022-07-21 14:54:12 -0300

    Primera Entrega Final

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -7,9 +7,6 @@ function Navbar () {
  return(
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <NavLink className="nav_link" to='/' >CinemaComics</NavLink>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
   <div className="navbarItems" >
     <ul className="navbar-nav" >
     <li className="nav-item active" > 
